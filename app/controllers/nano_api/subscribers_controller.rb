de7d9342aacd1a114fbfba7e148008485957c19b
Timur Kozmenko <timraell@gmail.com>
2013-05-08 13:20:21 +0700

    Add marker to creating of Subscriber

--- app/controllers/nano_api/subscribers_controller.rb
+++ app/controllers/nano_api/subscribers_controller.rb
@@ -2,6 +2,7 @@ module NanoApi
   class SubscribersController < NanoApi::ApplicationController
     def create
       @subscriber = NanoApi::Subscriber.new params[:subscriber]
+      @subscriber.marker = marker
 
       status = @subscriber.save ? :ok : :bad_request
       render nothing: true, status: status

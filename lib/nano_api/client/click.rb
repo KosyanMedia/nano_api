9b275ab047c7ed0e3421ef18c28c5703f1034ec5
Timur Kozmenko <timraell@gmail.com>
2013-05-13 19:19:13 +0700

    Add marker to all click API calls

--- lib/nano_api/client/click.rb
+++ lib/nano_api/client/click.rb
@@ -1,17 +1,17 @@
 module NanoApi::Client::Click
 
   def click search_id, order_url_id, params = {}
-    post('searches/%d/order_urls/%d' % [search_id, order_url_id], params).symbolize_keys
+    post('searches/%d/order_urls/%d' % [search_id, order_url_id], params.merge(marker: marker)).symbolize_keys
   rescue RestClient::ResourceNotFound
     nil
   end
 
   def link search_id, airline_id, params = {}
-    get("airline_logo/#{airline_id}", params.merge(search_id: search_id), json: false).symbolize_keys
+    get("airline_logo/#{airline_id}", params.merge(search_id: search_id, marker: marker), json: false).symbolize_keys
   end
 
   def deeplink search_id, proposal_id, params = {}
-    get('airline_deeplinks/%d' % proposal_id, params.merge(search_id: search_id)).symbolize_keys
+    get('airline_deeplinks/%d' % proposal_id, params.merge(search_id: search_id, marker: marker)).symbolize_keys
   end
 
 end

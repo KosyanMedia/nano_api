2bc033f0bde3d8488f38e00ff9c575676529577a
pyromaniac <kinwizard@gmail.com>
2013-06-26 20:25:44 +0700

    Fixed whitelabel api call normalizes response

--- lib/nano_api/client/white_label.rb
+++ lib/nano_api/client/white_label.rb
@@ -1,5 +1,5 @@
 module NanoApi::Client::WhiteLabel
   def white_label host
-    get('white_labels/show', signature: signature(nil, [host]), white_label_host: host)
+    get('white_labels/show', signature: signature(nil, [host]), white_label_host: host)['white_label']
   end
 end
--- spec/lib/nano_api/client/white_label_spec.rb
+++ spec/lib/nano_api/client/white_label_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe NanoApi::Client do
   describe '#white_label' do
     before { stub_request(:get, "http://test.te/white_labels/show.json?locale=en&signature=97e7f3e73591ab7fe0bb115244b09b3d&white_label_host=example.com").
-      to_return(:status => 200, :body => '{ "config": {} }') }
+      to_return(:status => 200, :body => '{ "white_label": { "config": {} } }') }
     specify { subject.white_label('example.com').should == { 'config' => {} } }
   end
 end

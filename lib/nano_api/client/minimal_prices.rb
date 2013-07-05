8fc0521c6ebd13166959d2e856faaadeb3301353
pyromaniac <kinwizard@gmail.com>
2013-07-05 13:29:57 +0700

    Added client modules requirement

--- lib/nano_api/client/minimal_prices.rb
+++ lib/nano_api/client/minimal_prices.rb
@@ -1,26 +1,30 @@
-module NanoApi::Client::MinimalPrices
-  def week_minimal_prices search_id, direct_date = nil, return_date = nil
-    get_raw('minimal_prices',
-      search_id: search_id, direct_date: direct_date, return_date: return_date
-    )
-  end
+module NanoApi
+  class Client
+    module MinimalPrices
+      def week_minimal_prices search_id, direct_date = nil, return_date = nil
+        get_raw('minimal_prices',
+          search_id: search_id, direct_date: direct_date, return_date: return_date
+        )
+      end
 
-  def month_minimal_prices search_id, month = nil
-    get_raw('month_minimal_prices', search_id: search_id, month: month)
-  end
+      def month_minimal_prices search_id, month = nil
+        get_raw('month_minimal_prices', search_id: search_id, month: month)
+      end
 
-  def nearest_cities_prices search_id
-    get_raw('nearest_cities_prices', search_id: search_id)
-  end
+      def nearest_cities_prices search_id
+        get_raw('nearest_cities_prices', search_id: search_id)
+      end
 
-  LATEST_PRICES_PER_PAGE = 30
+      LATEST_PRICES_PER_PAGE = 30
 
-  def latest_prices params
-    latest_prices_params = params.slice(
-      :origin, :destination, :origin_iata, :destination_iata, :period,
-      :trip_duration, :sorting, :one_way, :page, :currency, :show_to_affiliates, :period, :per_page
-    ).reverse_merge(show_to_affiliates: affiliate?, per_page: LATEST_PRICES_PER_PAGE)
+      def latest_prices params
+        latest_prices_params = params.slice(
+          :origin, :destination, :origin_iata, :destination_iata, :period,
+          :trip_duration, :sorting, :one_way, :page, :currency, :show_to_affiliates, :period, :per_page
+        ).reverse_merge(show_to_affiliates: affiliate?, per_page: LATEST_PRICES_PER_PAGE)
 
-    get('latest_prices', latest_prices_params).try(:symbolize_keys)
+        get('latest_prices', latest_prices_params).try(:symbolize_keys)
+      end
+    end
   end
 end

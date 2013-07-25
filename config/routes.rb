e0424dd514065761e8c532f86011cec2879cb9cb
Ramil Mustafin <rommel.rmm@gmail.com>
2013-07-25 13:02:32 +0700

    funk dat routes

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,7 @@ NanoApi::Engine.routes.draw do
   get '/week_minimal_prices' => 'minimal_prices#week', as: :week_minimal_prices
   get '/month_minimal_prices' => 'minimal_prices#month', as: :month_minimal_prices
   get '/nearest_cities_prices' => 'minimal_prices#nearest', as: :nearest_cities_prices
-  match '/latest_prices' => 'minimal_prices#latest_prices', via: [:get, :post]
+  get '/latest_prices' => 'minimal_prices#latest_prices'
+  post '/latest_prices' => 'minimal_prices#latest_prices'
   get '/estimated_search_duration' => 'gate_meta#search_duration', as: :estimated_search_duration
 end

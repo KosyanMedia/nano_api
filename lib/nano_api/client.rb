cf0f3a11a72350df44e4781cb5f7241752e9e305
Andrew Gridnev <andrew.gridnev@gmail.com>
2013-08-14 19:30:29 +0700

    Add mappings for es, it, fr locales to en locale

--- lib/nano_api/client.rb
+++ lib/nano_api/client.rb
@@ -15,7 +15,10 @@ module NanoApi
       :'en-NZ' => :en_AU,
       :'en-IN' => :en,
       :'en-SG' => :en,
-      :'en-CA' => :en
+      :'en-CA' => :en,
+      :fr => :en,
+      :it => :en,
+      :es => :en
     }
     DEFAULT_HOST_KEY = :nano_server
 

46a81514272a821350e9485750a8674eaaefb4f7
Ainur <nur.php@gmail.com>
2013-10-29 12:45:40 +0700

    remove first class from nano_api

--- app/helpers/searches_helper.rb
+++ app/helpers/searches_helper.rb
@@ -2,8 +2,7 @@ module SearchesHelper
   def trip_classes_map
     {
       t('nano_api.searches.helpers.trip_classes.economy_upcase') => 0,
-      t('nano_api.searches.helpers.trip_classes.business_upcase') => 1,
-      t('nano_api.searches.helpers.trip_classes.first_upcase') => 2
+      t('nano_api.searches.helpers.trip_classes.business_upcase') => 1
     }
   end
 

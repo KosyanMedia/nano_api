152de04e8edbb67bbf91b4ab753de9138a53c809
Exoth <yuri.trofimenko@aviasales.ru>
2013-10-31 20:11:42 +0700

    Add NanoApi::ApplicationController backend.

--- app/controllers/nano_api/application_controller.rb
+++ app/controllers/nano_api/application_controller.rb
@@ -1,16 +1,5 @@
 module NanoApi
-  class ApplicationController < ::ApplicationController
-    skip_before_filter :verify_authenticity_token
+  class ApplicationController < NanoApi::Backends::ApplicationController
     nano_extend
-
-  protected
-
-    def forward_json json, status = :ok
-      response.content_type = Mime::JSON
-      if defined?(Rollbar) && status != :ok
-        Rollbar.report_exception NanoApi::Client::RequestError.new(json), status: status
-      end
-      render text: json, status: status
-    end
   end
 end

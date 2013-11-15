9b3fabbb40e921d77ac011b8aa74f93ac96b325c
Pavel Gorlov <pavel0788@yandex.ru>
2013-11-15 20:17:46 +0700

    FIX option key

--- lib/nano_api/client/white_label.rb
+++ lib/nano_api/client/white_label.rb
@@ -5,7 +5,7 @@ module NanoApi
         white_label_params = {signature: signature(nil, [preview, host].compact), white_label_host: host}
         white_label_params[:preview] = preview if preview
 
-        get('white_labels/show', white_label_params, host_key: :travelpayouts_server)['white_label']
+        get('white_labels/show', white_label_params, host: :travelpayouts_server)['white_label']
       end
     end
   end

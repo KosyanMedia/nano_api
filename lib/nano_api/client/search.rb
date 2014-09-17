67f0b6f58d5c0c89efd6886f20a52c0e425dca46
Exoth <yuri.trofimenko@aviasales.ru>
2014-09-17 20:40:23 +0700

    Generalize country host overrides.

--- lib/nano_api/client/search.rb
+++ lib/nano_api/client/search.rb
@@ -4,15 +4,18 @@ module NanoApi
 
       def search params, options = {}
         path = NanoApi.config.search_path + (NanoApi.config.chain_prefix || '') + options[:chain]
+        user_ip = request.try(:remote_ip)
         search_params = params.symbolize_keys.merge(
           marker: controller.try(:marker),
-          user_ip: request.try(:remote_ip),
+          user_ip: user_ip,
         )
         search_params[:host] = request.host if request.try(:host).present?
-        # Send fake host for us users
-        if search_params[:host] == 'www.jetradar.com' && search_params[:user_ip] &&
-          CmsEngine.geoip.try(:country, search_params[:user_ip]).try(:country_code2) == 'US'
-          search_params[:host] = 'us.jetradar.com'
+
+        if user_ip &&
+          (country_host_override = CmsEngine::DomainConfig.current.country_host_override.with_indifferent_access) &&
+          (country_code = CmsEngine.geoip.try(:country, user_ip).try(:country_code2)) &&
+          (host_replacement = country_host_override[country_code])
+          search_params[:host] = host_replacement
         end
         post_raw(path, search_params, options.reverse_merge!(host: :search_server))
       rescue RestClient::Exception => exception
